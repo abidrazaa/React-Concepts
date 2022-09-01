c8d7cf23c12c523f35e1c0709e21883dc78bc08d
Abid Raza <abid.neduet@gmail.com>
2022-09-01 19:18:00 +0500

    lifting state up

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import Temperature from './components/class/lifting-state-up/CalculateTemperature';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your apppp!</Text>
+      {/* <Text>Open up App.js to start working on your apppp!</Text> */}
       <StatusBar style="auto" />
+      <Temperature /> 
     </View>
   );
 }
---
+++ components/class/lifting-state-up/InputTemperature.js
@@ -0,0 +1,31 @@
+import { View, Text, TextInput } from "react-native"
+import React, {Component} from 'react';
+
+class InputTemperature extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.props.changeTemperature(event)
+    }
+
+    render(){
+        return(
+            <View>
+                <TextInput 
+                    value={(this.props.temperature).toString()}
+                    // onChange = {this.handleChange(e)}
+                    onChangeText={text => this.handleChange(text)}
+
+                />
+                <Text>The temperature is {this.props.temperature}</Text>
+            </View>
+        )
+    }
+
+}
+
+export default InputTemperature;
