4a60ba4052cafb450285a4e7e926bfea09eba2db
Abid Raza <abid.neduet@gmail.com>
2022-09-10 22:00:54 +0500

    react concepts

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowName from './components/useState';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <ShowName />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
